d49b1920257490290cbeffbdcfa576aac3f30e98
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-04-26 16:43:16 +0900

    사칙연산 기호를 받아 String으로 저장

--- Desktop/BackEndStudy/PersonalProject/Calculator/src/App.java
+++ Desktop/BackEndStudy/PersonalProject/Calculator/src/App.java
@@ -9,6 +9,9 @@ public class App {
         System.out.println("첫 번째 숫자를 입력하세요: ");
         double firstInput = Double.parseDouble(br.readLine());
 
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        String operations = br.readLine();
+
         System.out.println("두 번째 숫자를 입력하세요: ");
         double secoundInput = Double.parseDouble(br.readLine());
     }
