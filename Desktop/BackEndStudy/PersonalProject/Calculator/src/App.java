c95337aded28f22c77ef22620f99da0af4e0a460
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-04-26 16:51:55 +0900

    입력받은 정수들과 사칙연산 기호를 사용하여 연산을 진행 후 출력

--- Desktop/BackEndStudy/PersonalProject/Calculator/src/App.java
+++ Desktop/BackEndStudy/PersonalProject/Calculator/src/App.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 public class App {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Double result = 0.0; // 계산 결과 값
 
         System.out.println("첫 번째 숫자를 입력하세요: ");
         double firstInput = Double.parseDouble(br.readLine());
@@ -14,5 +15,26 @@ public class App {
 
         System.out.println("두 번째 숫자를 입력하세요: ");
         double secoundInput = Double.parseDouble(br.readLine());
+
+        if (operations.equals("/") && secoundInput == 0) {
+            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+        } else {
+            switch (operations) {
+                case "+":
+                    result = firstInput + secoundInput;
+                    break;
+                case "-":
+                    result = firstInput - secoundInput;
+                    break;
+                case "/":
+                    result = firstInput / secoundInput;
+                    break;
+                case "*":
+                    result = firstInput * secoundInput;
+                    break;
+            }
+
+            System.out.println("결과: " + result);
+        }
     }
 }
