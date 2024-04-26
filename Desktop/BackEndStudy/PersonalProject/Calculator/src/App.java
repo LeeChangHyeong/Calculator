1931fb65473ead79e9ace310bb05be99c06a485a
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-04-26 16:58:34 +0900

    반복문을 사용하여 exit를 입력하지 않으면 무한으로 계산을 진행할 수 있게 구현

--- Desktop/BackEndStudy/PersonalProject/Calculator/src/App.java
+++ Desktop/BackEndStudy/PersonalProject/Calculator/src/App.java
@@ -5,36 +5,44 @@ import java.io.InputStreamReader;
 public class App {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Double result = 0.0; // 계산 결과 값
 
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        double firstInput = Double.parseDouble(br.readLine());
+        while (true) {
+            Double result = 0.0; // 계산 결과 값
 
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        String operations = br.readLine();
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            double firstInput = Double.parseDouble(br.readLine());
 
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        double secoundInput = Double.parseDouble(br.readLine());
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            String operations = br.readLine();
 
-        if (operations.equals("/") && secoundInput == 0) {
-            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-        } else {
-            switch (operations) {
-                case "+":
-                    result = firstInput + secoundInput;
-                    break;
-                case "-":
-                    result = firstInput - secoundInput;
-                    break;
-                case "/":
-                    result = firstInput / secoundInput;
-                    break;
-                case "*":
-                    result = firstInput * secoundInput;
-                    break;
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            double secoundInput = Double.parseDouble(br.readLine());
+
+            if (operations.equals("/") && secoundInput == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            } else {
+                switch (operations) {
+                    case "+":
+                        result = firstInput + secoundInput;
+                        break;
+                    case "-":
+                        result = firstInput - secoundInput;
+                        break;
+                    case "/":
+                        result = firstInput / secoundInput;
+                        break;
+                    case "*":
+                        result = firstInput * secoundInput;
+                        break;
+                }
+                System.out.println("결과: " + result);
             }
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료) ");
+            String exit = br.readLine();
 
-            System.out.println("결과: " + result);
+            if (exit.equals("exit")) {
+                break;
+            }
         }
     }
 }
